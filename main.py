4bdebd32e9274e4a01ec11facb6a2fe638e0fd0c
pivenappq <111285146+pivenappq@users.noreply.github.com>
2022-10-26 15:21:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import pandas as pd
 from datetime import timedelta
 from uuid import uuid4
 
-def get_groups():
-    df = pd.read_csv('TEST_DATASET.csv', delimiter=',')
+def get_groups(df):
     gb = df.groupby('customer_id')
     # session_id = uuid4()
     groups = []
     for name in gb.groups:
-        df = gb.get_group(name).sort_values('timestamp', ascending=True).reset_index(drop=True)
-        groups.append(sort_sessions(df))
+        cient = gb.get_group(name).sort_values('timestamp', ascending=True).reset_index(drop=True)
+        groups.append(sort_sessions(cient))
     df = pd.concat(groups).reset_index(drop=True)
     return df
 
@@ -30,7 +29,9 @@ def sort_sessions(group: pd.DataFrame) -> pd.DataFrame:
             group.at[i, 'session_id'] = session_id
     return group
 
-df = get_groups()
+
+raw_df = pd.read_csv('TEST_DATASET.csv', delimiter=',')
+df = get_groups(raw_df)
 
 
 
